b10b3aa3c6a7ee92f56dfc88f9767442d6b80569
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-23 16:25:31 +0200

    User model

---
+++ app/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import ForeignKey
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(length=255))
+    email: Mapped[str] = mapped_column(String(length=255))
+
+    def __repr__(self) -> str:
+        return self.username
