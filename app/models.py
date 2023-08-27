0f9368fded044c39f8268138439537cb1a6f0710
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-27 15:49:31 +0200

    A lot of new models
    Recommendation, FictionType, Tag

--- app/models.py
+++ app/models.py
@@ -1,8 +1,13 @@
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy import ForeignKey
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import Table
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 from . import Base
 
@@ -15,5 +20,71 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(length=255), unique=True)
     password: Mapped[str] = mapped_column(String(length=300))
 
+    recommendations: Mapped[list["Recommendation"]] = relationship(
+        back_populates="fiction_type", cascade="all, delete-orphan"
+    )
+
     def __repr__(self) -> str:
         return self.username
+
+
+class FictionType(Base):
+    __tablename__ = 'fiction_type'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(length=255), unique=True)
+    slug: Mapped[str] = mapped_column(String(length=300), unique=True)
+
+    recommendations: Mapped[list["Recommendation"]] = relationship(
+        back_populates="fiction_type", cascade="all, delete-orphan"
+    )
+
+    def __repr(self) -> str:
+        return self.name
+
+
+tagged_recommendations = Table(
+    'tagged_recommendations',
+    Base.metadata,
+    Column('recommendation_id', ForeignKey(
+        'recommendation.id'), primary_key=True),
+    Column('tag_id', ForeignKey('tag.id'), primary_key=True)
+)
+
+
+class Recommendation(Base):
+    __tablename__ = 'recommendation'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(length=255))
+    short_description: Mapped[str] = mapped_column(String())
+    opinion: Mapped[str] = mapped_column(String())
+    fiction_type_id: Mapped[int] = mapped_column(ForeignKey('fiction_type.id'))
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    published: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), default=datetime.utcnow())
+    updated: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=True)
+    tags: Mapped[list['Tag']] = relationship(secondary=tagged_recommendations,
+                                             cascade="all, delete-orphan")
+
+    fiction_type: Mapped[FictionType] = relationship(
+        back_populates='recommendations')
+
+    user: Mapped["User"] = relationship(
+        back_populates="recommendations"
+    )
+
+    def __repr__(self) -> str:
+        return self.title
+
+
+class Tag(Base):
+    __tablename__ = 'tag'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(length=255))
+    slug: Mapped[str] = mapped_column(String(length=300))
+
+    def __repr__(self) -> str:
+        return self.name
